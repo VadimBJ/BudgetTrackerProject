3e9a04b2e1fa60e6fe40e56dd39a68c0fa563e98
Vadim Balanciuc <b.vadik2017@gmail.com>
2023-04-18 18:13:54 +0200

    refactor Category

--- src/Category.java
+++ src/Category.java
@@ -1,19 +1,9 @@
 public class Category {
-  private static int counter = 0;
-  private final int id;
+
   private String title;
 
   public Category(String title) {
-    id = ++counter;
     this.title = title;
-  }
-
-  @Override
-  public String toString() {
-    return "Category{" +
-        "id=" + id +
-        ", title='" + title + '\'' +
-        '}';
   }
 
   public String getTitle() {
